20fcfb24e6525e04523ba29531f02532e65e37d6
Shane Misra <shane.misra@gmail.com>
2022-11-14 11:50:10 -0700

    Complete challenge1.js

---
+++ challenge1.js
@@ -0,0 +1,26 @@
+ // "Given an array of strings, return the strings that have exactly four characters."
+
+ // Overall goal - 
+ // 1. Define a new array, populate it with multiple strings - some of which have exactly four characters and others that do not.
+ // 2. Begin a 'for loop structure', initializing it at the beginning of the array and run it until it loops through each index position within the array, with each cycle progressing through the array by an increment of 1. 
+ // 3. Create an if statement within the array that compares the length attribute of the value at the current index position within the array to the number 4. If the value is strictly equal to 4 (=== operator), use that boolean value to:
+ // If true, print the string to the console.
+ // If false, do nothing and continue looping through the statement until the array has been cycled through fully.
+
+
+// Pseudocode:
+// var array = ["words1", "words2","words3", "words4", "words5", ]
+
+// for (var i = 0; i < array.length; i++) {
+//  if (array[i].length === 4) {
+//    console.log(array[i])
+//}
+
+//FINAL SOLUTION //
+
+var names = ["Shane", "Steve", "Gary", "Jonas", "Mike", "Mary", "John", "Bill", "Ariadne", "Aretha", "Gerhardt",]
+for (var i = 0; i<names.length; i++) {
+    if (names[i].length ===4) {
+        console.log(names[i])
+    }
+}
